37d1c727a808e270e8820555a31542906b6e02e3
orlando <orlando.burli@gmail.com>
2023-01-07 09:53:27 -0400

    feat: add read command features

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include "input/input_buffer.h"
+#include "prompt/prompt_helper.h"
+#include "input/input_reader.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    InputBuffer* inputBuffer = new_input_buffer();
+
+    while (true) {
+        prompt_print();
+
+        read_input(inputBuffer);
+
+        if (strcmp(inputBuffer->buffer, ".exit") == 0) {
+            close_input_buffer(inputBuffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'", inputBuffer->buffer);
+        }
+    }
 
     return 0;
 }
