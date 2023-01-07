9b49372ec0c1c59b0f1ed6794894da70be5e5e53
orlando <orlando.burli@gmail.com>
2023-01-07 10:09:07 -0400

    feat: command exit handler

---
+++ commands/command_exit.c
@@ -0,0 +1,5 @@
+//
+// Created by Orlando Burli Júnior on 07/01/23.
+//
+
+#include "command_exit.h"
---
+++ commands/command_exit.h
@@ -0,0 +1,8 @@
+//
+// Created by Orlando Burli Júnior on 07/01/23.
+//
+
+#ifndef BURLIDB_COMMAND_EXIT_H
+#define BURLIDB_COMMAND_EXIT_H
+
+#endif //BURLIDB_COMMAND_EXIT_H
