645200438d291ecf0b043b849c42f5e914ca02ac
orlando <orlando.burli@gmail.com>
2023-01-07 09:53:10 -0400

    feat: add prompt helper functions

---
+++ prompt/prompt_helper.c
@@ -0,0 +1,12 @@
+//
+// Created by Orlando Burli Júnior on 07/01/23.
+//
+
+#include <stdio.h>
+#include "prompt_helper.h"
+#include "../constants.h"
+
+
+void prompt_print() {
+    printf("%s > ", PROMPT_TITLE);
+}
---
+++ prompt/prompt_helper.h
@@ -0,0 +1,10 @@
+//
+// Created by Orlando Burli Júnior on 07/01/23.
+//
+
+#ifndef BURLIDB_PROMPT_HELPER_H
+#define BURLIDB_PROMPT_HELPER_H
+
+void prompt_print();
+
+#endif //BURLIDB_PROMPT_HELPER_H
